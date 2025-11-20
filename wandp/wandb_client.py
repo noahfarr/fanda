35cfcc51519e511166dc33888210ccae5fa0f921
noahfarr <login@noahfarr.com>
2025-11-20 16:59:39 +0100

    Initial commit

---
+++ wandp/wandb_client.py
@@ -0,0 +1,31 @@
+import wandb
+import pandas as pd
+from typing import Optional
+
+
+def fetch_history(
+    api: wandb.Api,
+    entity: str,
+    project: str,
+    keys: Optional[list[str]] = None,
+    filters: Optional[dict] = None,
+    samples: int = 500,
+) -> pd.DataFrame:
+    runs = api.runs(f"{entity}/{project}", filters=filters)
+
+    histories = runs.histories(
+        samples=samples,
+        keys=keys,
+        format="pandas"
+    )
+    configs = pd.DataFrame([{"run_id": run.id, "run_name": run.name} for run in runs])
+
+    df = pd.merge(histories, configs, on="run_id", how="left")
+
+    return df
+
+if __name__ == "__main__":
+    api = wandb.Api()
+    df = fetch_history(api, "noahfarr", "debug")
+    print(df)
+
